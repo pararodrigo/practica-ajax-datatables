98e0682a8297200bff42da70da36f217062a56c9
rodrigo <pararodrigo_bd@hotmail.com>
2015-03-24 12:02:58 +0100

    esta version funciona medianamente bien
    Falta sacar los datos de la tabla y meterlos al formulario

--- aplicacion/app/scripts/main.js
+++ aplicacion/app/scripts/main.js
@@ -1,82 +1,162 @@
 
-
+var miTabla;
 $(document).ready(function() {
 
+//////////////////////////CARGAR LISTA DE CLINICAS EN EL FORMULARIO/////////////
+
+    var clinicas = $.ajax({
+        url: 'http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ/clinicas.php',
+        type: 'GET',
+        dataType: 'json'
+
+    })
+        .done(function(data) {
+            $.each(data, function(index) {
+                $('#inputClinicas').append('<option value="'+data[index].id_clinica+'">' + data[index].nombre + '</option>');
+            });
+        })
+
+        .fail(function() {
+            console.log("error al cargar la lista de clinicas");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+
+///////////////////////////////CARGO LA TABLA///////////////////////////////////////
+
+     miTabla = $('#example').dataTable( {
 
 
-    $('#example').dataTable( {
+        ajax:"http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ/datos.php",
 
-        ajax: "http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ_2/php.php",
+
         columns:[
             {data:"id_doctor"},
-            {data:"nombre"},
+            {data:"nombre_doctor",
+                render:function(data){
+                    return'<a class="editar">'+data+'</a>'
+                }
+            },
             {data:"numcolegiado"},
             {
-              data:null,
-              defaultContent:'clinicas'
+              data:"clinicas",
+              render:function(data){
+                  return '<ul><li>'+data+'</li></ul>'
+              }
             },
             {
-                data:null,
-                className:'center',
-                defaultContent:'<button id="editar" class="btn btn-primary btn-sm">Editar</button>'
+                data: 'id_doctor',
+                render: function (data) {
+                    return '<button class="editar btn btn-primary btn-sm" value="' + data + '">Editar</button>'
+                }
             },
             {
-                data:null,
-                className:'center',
-                defaultContent:'<button id="borrar" class="btn btn-warning btn-sm">Borrar</button>'
+                data: 'id_doctor',
+                render: function (data) {
+                    return '<button class="borrar btn btn-warning btn-sm" value="' + data + '">Borrar</button>'
+                }
             }
         ],
         language:{
             url:"//cdn.datatables.net/plug-ins/f2c75b7247b/i18n/Spanish.json"
         },
+
         columnDefs:[{
             targets:[0],
             visible:false
         }]
+
     } );
 //////////////////////////VENTANAS EMERGENTES///////////////////////////////
 
-    $('table').on('click','tr td #editar',function(){
+    $('table').on('click','.editar',function(){
+
+        var nRow = $(this).parents('tr')[0];
+        var aData = miTabla.row(nRow).data();
+        //var fila = nRow.rowIndex;
+        //var aData = miTabla.row(fila).data();
+
+        //var colegiado = 'num colegiado'
+
+        $('#inputNombre').val(aData.nombre_doctor);
+        $('#inputColegiado').val(aData.numcolegiado);
+
         $('#forEditar').modal('show');
 
+        $('#ConfirmaGuardar').on('click',function(){
+                var doc_id = $('#inputID').val();
+                var clinicasSelect = $('#inputClinicas').val();
+                var nombreDocotor = $('#inputNombre').val();
+                var numColegiado = $('#inputColegiado').val();
+
+            $.ajax({
+                type: 'POST',
+                dataType:'json',
+                url:'http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ/modificar_doctores.php',
+                data:{
+                    idDoctor: doc_id,
+                    nombreDoctor: nombreDocotor,
+                    numColegiado: numColegiado,
+                    clinicasSelect: clinicasSelect
+                },
+                error: function (xhr, status, error) {
+                    //mostraríamos alguna ventana de alerta con el error
+                    alert("Ha entrado en error");
+                },
+                success: function (data) {
+                    //obtenemos el mensaje del servidor, es un array!!!
+                    //var mensaje = (data["mensaje"]) //o data[0], en función del tipo de array!!
+                    //actualizamos datatables:
+                    /*para volver a pedir vía ajax los datos de la tabla*/
+                    //miTabla.fnDraw();
+                    miTabla.fnReloadAjax("http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ/datos.php");
+
+                    $('#forEditar').modal('hide');
+                }
+            })
+
+        })
+
+
     });
 
-    $('table').on('click','tr td #borrar',function() {
+    $('table').on('click','.borrar',function() {
 
-        /*
-        $('table tr').each(function(index){
-        var nombre = $(this).find("td").eq(0).html();
-        $('#idDoctor').html(nombre);
-        });
-        */
-        $('table tbody tr').each(function(index){
-        var nombre = $(this).closest('tr').find("td:nth-child(1)").html();
-        $('#idDoctor').html(nombre);
-        });     
 
+        var id_doctor = $(this).prop('value');
+        $('#idDoctor').html(id_doctor);
         $('#forBorrar').modal('show');
-    });
-//////////////////////////CARGAR LISTA DE CLINICAS EN EL FORMULARIO/////////////
-    
-    $.ajax({
-        url: 'http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ_2/clinicas.php',
-        type: 'GET',
-        dataType: 'json',
-        data: {param1: 'value1'},
-    })
-    .done(function(data) {
-            $.each(data, function(index) {
-                $('#inputClinicas').append('<option value="'+data[index].id_clinica+'">' + data[index].nombre + '</option>');
-            });
-    })
 
-    .fail(function() {
-        console.log("error al cargar la lista de clinicas");
-    })
-    .always(function() {
-        console.log("complete");
+        $('#ConfirmaBorrar').on('click',function(){
+            $.ajax({
+                /*en principio el type para api restful sería delete pero no lo recogeríamos en $_REQUEST, así que queda como POST*/
+                type: 'POST',
+                dataType: 'json',
+                url: 'http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ/borrar_doctor.php',
+                //estos son los datos que queremos actualizar, en json:
+                data: {
+                    id_doctor: id_doctor
+                },
+                error: function (xhr, status, error) {
+                    //mostraríamos alguna ventana de alerta con el error
+                    alert("Ha entrado en error");
+                },
+                success: function (data) {
+                    //obtenemos el mensaje del servidor, es un array!!!
+                    //var mensaje = (data["mensaje"]) //o data[0], en función del tipo de array!!
+                    //actualizamos datatables:
+                    /*para volver a pedir vía ajax los datos de la tabla*/
+                    //miTabla.fnDraw();
+                      miTabla.fnReloadAjax("http://localhost:8888/TAREA_DWEC_DATATABLES_AXAJ/datos.php");
+
+                    $('#forBorrar').modal('hide');
+                }
+            })
+        });
     });
-////////////////////////// METODO PARA OBTENER EL DOCTOR DE LA LISTA/////////////////
+
+
           
     
 } );
